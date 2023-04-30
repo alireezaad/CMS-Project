4fdb9c6ddad44c3851cf97ba84e521d27f557bc8
Alireza abasi <alireezaad@gmail.com>
2023-04-30 16:52:46 +0430

    feat: implement an interface and child class of that interface in repository layer for each model.

---
+++ CMS.RepositoryLayer/ArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using CMS.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.RepositoryLayer
+{
+    class ArticleRepository : GenericRepository<Article>, IArticleRepository
+    {
+        public ArticleRepository(CMSContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ CMS.RepositoryLayer/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CMS.ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.RepositoryLayer
+{
+    class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(CMSContext context) : base(context)
+        {
+        }
+    }
+}
